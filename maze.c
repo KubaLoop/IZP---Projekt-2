8907f660d12484024ac67c7e1359f79b0eccfa9b
Jakub <116542124+KubaLoop@users.noreply.github.com>
2023-11-27 10:35:38 +0100

    CHOPSUEY PICO (shorttest se fakt konat nebude)

--- maze.c
+++ maze.c
@@ -8,9 +8,9 @@ Projekt 2 - bludiště
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 /*
 TODO:
-2) Implementace argv programu
 3) Validace matice
 4) bonus řešení short test
 */
@@ -23,6 +23,27 @@ typedef struct
     unsigned char *cells;
 } Map;
 
+// 2. Podúkol - Kontrola hrany
+bool isborder(Map *map, int r, int c, int border)
+{
+    unsigned char test = map->cells[r * map->cols + c];
+    if (border == 1) // Levá hranice
+    {
+        if (test == 1 || test == 3 || test == 5 || test == 7)
+            return true;
+    }
+    else if (border == 2) // Horní/dolní hranice
+    {
+        if (test == 4 || test == 5 || test == 6 || test == 7)
+            return true;
+    }
+    else if (border == 3) // Pravá hranice
+    {
+        if (test == 2 || test == 3 || test == 6 || test == 7)
+            return true;
+    }
+    return false;
+}
 // Funkce pro alokaci mapy a její vytvoření ze souboru.
 Map *map_ctor(char *filename)
 {
@@ -49,30 +70,65 @@ Map *map_ctor(char *filename)
                         }
                         else
                         {
-                            printf("Chyba načtení jednotlivých hodnot.\n");
-                            break;
+                            break; // Při chybě načtení jednotlivých políček
                         }
                     }
                 }
             }
             else
             {
-                printf("Chyba alokace paměti.\n");
                 fclose(ptr);
+                free(map->cells);
                 free(map);
             }
         }
     }
-
+    else
+    {
+        fclose(ptr);
+        free(map);
+        return NULL;
+    }
     fclose(ptr);
     if (check == map->cols * map->rows)
+    {
+        for (int i = 0; i < map->rows; i++)
+        {
+            for (int j = 0; j < map->cols - 1; j++)
+            {
+                if ((isborder(map, i, j, 3) == true && isborder(map, i, j + 1, 1) == false) || (isborder(map, i, j, 3) == false && isborder(map, i, j + 1, 1) == true))
+                {
+                    free(map->cells);
+                    free(map);
+                    return NULL;
+                }
+            }
+        }
+        for (int i = 0; i < map->rows - 1; i++)
+        {
+            for (int j = 0; j < map->cols; j++)
+            {
+                if ((i % 2 == 0 && j % 2 == 1) || (i % 2 == 1 && j % 2 == 0))
+                {
+                    if ((isborder(map, i, j, 2) == true && isborder(map, i + 1, j, 2) == false) || (isborder(map, i, j, 2) == false && isborder(map, i + 1, j, 2) == true))
+                    {
+                        free(map->cells);
+                        free(map);
+                        return NULL;
+                    }
+                }
+            }
+        }
         return map;
+    }
     else
         return NULL;
 }
 // Funkce pro zničení mapy a uvolnění alokované paměti
 void map_dtor(Map *map)
 {
+    map->cols = 0;
+    map->rows = 0;
     free(map->cells);
     free(map);
 }
@@ -91,40 +147,20 @@ void map_print(Map *map)
         }
     }
 }
-// 2. Podúkol - Kontrola hrany
-bool isborder(Map *map, int r, int c, int border)
-{
-    unsigned char test = map->cells[r * map->cols + c];
-    if (border == 1) // Levá hranice
-    {
-        if (test == 1 || test == 3 || test == 5 || test == 7)
-            return true;
-    }
-    else if (border == 2) // Horní/dolní hranice
-    {
-        if (test == 4 || test == 5 || test == 6 || test == 7)
-            return true;
-    }
-    else if (border == 3) // Pravá hranice
-    {
-        if (test == 2 || test == 3 || test == 6 || test == 7)
-            return true;
-    }
-    return false;
-}
 
 // 3. Podúkol - Startovní hrana podle podmínek ze zadání
 // Funkce vrací hodnotu počáteční hranice 1 - Levá, 2 - Spodní/dolní, 3 - Pravá
+// Pokud do políčka nešlo vstoupit, tak vrátí 0
 int start_border(Map *map, int r, int c, int leftright)
 {
-    if (r % 2 == 0 && isborder(map, r, c, 1) == false)
+    if (r % 2 == 0 && isborder(map, r, c, 1) == false && c == 0)
     {
         if (leftright == 2)
             return 3;
         else
             return 2;
     }
-    else if (r % 2 == 1 && isborder(map, r, c, 1) == false)
+    else if (r % 2 == 1 && isborder(map, r, c, 1) == false && c == 0)
     {
         if (leftright == 2)
             return 2;
@@ -132,28 +168,28 @@ int start_border(Map *map, int r, int c, int leftright)
             return 3;
     }
 
-    else if (((c % 2 == 0 && r % 2 == 0) || (c % 2 == 1 && r % 2 == 1)) && isborder(map, r, c, 3) == false)
+    else if (((c % 2 == 0 && r % 2 == 0) || (c % 2 == 1 && r % 2 == 1)) && isborder(map, r, c, 3) == false && c + 1 == map->cols)
     {
         if (leftright == 2)
             return 2;
         else
             return 1;
     }
-    else if (((c % 2 == 0 && r % 2 == 1) || (c % 2 == 1 && r % 2 == 0)) && isborder(map, r, c, 3) == false)
+    else if (((c % 2 == 0 && r % 2 == 1) || (c % 2 == 1 && r % 2 == 0)) && isborder(map, r, c, 3) == false && c + 1 == map->cols)
     {
         if (leftright == 2)
             return 1;
         else
             return 2;
     }
-    else if (c % 2 == 0 && r % 2 == 0 && isborder(map, r, c, 2) == false)
+    else if (((c % 2 == 0 && r % 2 == 0) || (c % 2 == 1 && r % 2 == 1)) && isborder(map, r, c, 2) == false && r == 0)
     {
         if (leftright == 2)
             return 1;
         else
             return 3;
     }
-    else if (c % 2 == 0 && r % 2 == 1 && isborder(map, r, c, 2) == false)
+    else if (((c % 2 == 1 && r % 2 == 0) || (c % 2 == 0 && r % 2 == 1)) && isborder(map, r, c, 2) == false && r + 1 == map->cols)
     {
         if (leftright == 2)
             return 3;
@@ -164,16 +200,19 @@ int start_border(Map *map, int r, int c, int leftright)
 }
 
 // Vlastní funkce pro hledání cesty.
+// Funkce postupně printuje cestu, kudy se prochází bludiště.
 // Složité podmínky vznikly postupným bugfixováním při hledání cesty podle zadání.
 void map_path(Map *map, int r, int c, int border, int leftright)
 {
     bool move = false;
     bool end = false;
     int lastborder = 0;
+    int iter = 0;
     while (end == false)
     {
         printf("%d,%d\n", r + 1, c + 1);
         move = false;
+        iter++;
         while (move == false)
         {
             if (isborder(map, r, c, border) == false)
@@ -243,14 +282,14 @@ void map_path(Map *map, int r, int c, int border, int leftright)
                 {
                     if (((c % 2 == 1) && (r % 2 == 1)) || ((c % 2 == 0) && (r % 2 == 0)))
                     {
-                        if ((lastborder != 3 && leftright == 1) || leftright == 2)
+                        if ((lastborder != 3 && leftright == 1 && isborder(map, r, c, 1) == false) || leftright == 2)
                             border = 1;
                         else
                             border = 3;
                     }
                     else
                     {
-                        if ((leftright == 1 && lastborder == 1) && (((c % 2 == 1) && (r % 2 == 0)) || ((c % 2 == 0) && (r % 2 == 1))))
+                        if (((leftright == 1 && lastborder == 1) && (((c % 2 == 1) && (r % 2 == 0)) || ((c % 2 == 0) && (r % 2 == 1)))) || isborder(map, r, c, 3) == true)
                             border = 1;
                         else
                             border = 3;
@@ -259,7 +298,7 @@ void map_path(Map *map, int r, int c, int border, int leftright)
                 }
                 else if (border == 3)
                 {
-                    if (((((c % 2 == 1) && (r % 2 == 1)) || ((c % 2 == 0) && (r % 2 == 0))) && leftright == 2) || (leftright == 1 && lastborder != 2))
+                    if (((((c % 2 == 1) && (r % 2 == 1)) || ((c % 2 == 0) && (r % 2 == 0))) && leftright == 2) || (leftright == 1 && lastborder != 2 && iter != 1))
                         border = 2;
                     else
                         border = 1;
@@ -273,21 +312,98 @@ void map_path(Map *map, int r, int c, int border, int leftright)
         {
             end = true;
         }
+        if (iter > 1000)
+        {
+            end = true;
+            printf("Algoritmus prekrocil 1000 iteraci - program byl ukoncen.");
+        }
     }
 }
-int main() // int argc, char *argv[]
+int main(int argc, char *argv[])
 {
-    Map *map = map_ctor("soubor.txt");
-    if (map == NULL)
-        return -1;
+    if (argc == 2)
+    {
+        char str[] = "--help";
+        if (strcmp(str, argv[1]) == 0)
+        {
+            printf("Program lze spustit s nasledujicimi parametry:\n");
+            printf("1) ./maze --test nazevsouboru.txt vytiskne Valid, pokud je soubor platny jinak Invalid.\n");
+            printf("2) ./maze --rpath R C nazevsouboru.txt vytiskne postupnou cestu bludištěm podle pravidla prave ruky.\n");
+            printf("3) ./maze --lpath R C nazevsouboru.txt vytiskne postupnou cestu bludištěm podle pravidla leve ruky.\n");
+            printf("R je radková a C sloupcova souradnice pocatecniho bodu.\n");
+        }
+        else
+            printf("Invalid syntax.\n");
+    }
 
-    int leftright = 2;
+    else if (argc == 3)
+    {
+        char str[] = "--test";
+        if (strcmp(str, argv[1]) == 0)
+        {
+            Map *map = map_ctor(argv[2]);
+            if (map == NULL)
+            {
+                printf("Invalid\n");
+            }
+            else
+            {
+                printf("Valid\n");
+                map_dtor(map);
+            }
+        }
+        else
+            printf("Invalid syntax.\n");
+    }
+    else if (argc == 5)
+    {
+        char str1[] = "--rpath";
+        char str2[] = "--lpath";
+        int leftright;
+        if (strcmp(str1, argv[1]) == 0)
+        {
+            leftright = 2;
+        }
+        else if (strcmp(str2, argv[1]) == 0)
+        {
+            leftright = 1;
+        }
+        else
+        {
+            printf("Invalid syntax.\n");
+            return -1;
+        }
 
-    map_print(map);
+        Map *map = map_ctor(argv[4]);
+        if (map == NULL)
+        {
+            printf("Chybny soubor nebo je prazdny.\n");
+            return -1;
+        }
+        int r = atoi(argv[2]);
+        int c = atoi(argv[3]);
+        if (r == 0 || c == 0)
+        {
+            printf("Chybne zadane souradnice!\n");
+            return -1;
+        }
+        r--;
+        c--;
 
-    int border = start_border(map, 5, 0, leftright);
-    map_path(map, 5, 0, border, leftright);
-    map_dtor(map);
-
+        // map_print(map);
+        int border = start_border(map, r, c, leftright);
+        if (border == 0)
+        {
+            printf("Nelze vstoupit na tomto policku!");
+            return -1;
+        }
+        map_path(map, r, c, border, leftright);
+        map_dtor(map);
+    }
+    else
+    {
+        printf("Invalid syntax.\n");
+        printf("Zkuste spustit './maze --help'\n");
+    }
     return 0;
 }
